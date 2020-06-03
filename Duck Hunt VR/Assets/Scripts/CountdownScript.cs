68e7934c4e53b3191d4dda21aa2928ae695fed43
mitchsayer <mitchellbsayer@gmail.com>
2020-06-03 16:34:05 +1000

    Script Upload V1.0.8

---
+++ Duck Hunt VR/Assets/Scripts/CountdownScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountdownScript : MonoBehaviour
+{
+    [SerializeField] private Text uiText;
+    [SerializeField] private float mainTimer;
+
+    private float timer;
+    private bool canCount = true;
+    private bool doOnce = false;
+
+    void Start()
+    {
+        timer = mainTimer;
+	}
+
+    void Update()
+    {
+        if (timer >= 0f && canCount)
+        {
+            timer -= Time.deltaTime;
+            uiText.text = timer.ToString("0");
+		}
+
+        else if (timer <= 0f && !doOnce)
+        {
+            canCount = false;
+            doOnce = true;
+            uiText.text = "0";
+            timer = 0f;
+		}
+    }
+}
